31df54547a88231a6f58448d5ec116edfc3bca0a
Freddy-ux <alfredmuleri569@gmail.com>
2023-03-22 23:23:49 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Write your solution here!
+// My Code
+let cats = ['Milo', 'Otis', 'Garfield']
+function destructivelyAppendCat(name){
+    cats.push(name)
+    return cats
+}
+destructivelyAppendCat('Ralph')
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+    return cats
+}
+destructivelyPrependCat('Bob')
+// Removes Last element in the array
+function destructivelyRemoveLastCat(){
+    let destructivelyRemoveLastCat = cats.pop()
+    return destructivelyRemoveLastCat
+}
+destructivelyRemoveLastCat()
+// Removes Last element in the array
+function destructivelyRemoveFirstCat(){
+    let destructivelyRemoveFirstCat = cats.shift()
+    return destructivelyRemoveFirstCat
+}
+destructivelyRemoveFirstCat()
+/* appends a cat to the cats array
+   and returns a new array,
+   leaving the cats array unchanged */
+function appendCat(name){
+    const catArr = [...cats, name]
+    return catArr
+}
+appendCat('Broom')
+/* prepends a cat to the cats array and returns
+   new array, leaving the cats array unchanged */
+function prependCat(name){
+    const catArr = [name, ...cats]
+    return catArr
+}
+prependCat('Arnold')
+/* removes the first cat in the cats array
+   and returns a new array, leaving the cats
+   array unchanged */
+function removeFirstCat(){
+    const firstItem = cats.slice(1)
+    return firstItem
+}
+removeFirstCat()
+/* removes the first cat in the cats array
+   and returns a new array, leaving the cats
+   array unchanged */
+function removeLastCat(){
+    const lastItem = cats.slice(0,2)
+    return lastItem
+}
+removeLastCat()
